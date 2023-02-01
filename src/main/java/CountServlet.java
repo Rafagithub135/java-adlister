feb1b20fc73b1abce1ae33c55f3efe538f5acece
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-02-01 17:37:49 -0500

    removed comments

--- src/main/java/CountServlet.java
+++ src/main/java/CountServlet.java
@@ -7,16 +7,12 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 
-//    Create a page view counter
-//        Create a page that displays a number that goes up by one every time the /count page is viewed.
-
 @WebServlet(name = "CountServlet", urlPatterns = "/count")
 public class CountServlet extends HttpServlet {
     int count = 1;
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
             IOException {
-
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
         out.println("<h1>Count: " + count + "</h1>");
