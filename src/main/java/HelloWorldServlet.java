9f0f2ae520f8810945ef6695a11f94f24713ded3
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-02-01 16:33:33 -0500

    entered code from lecture

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,26 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import javax.servlet.*;
 
+
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        int count;
+        try {
+            count = Integer.parseInt(req.getParameter("count"));
+        } catch (Exception e) {
+            count = 1;
+        }
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        for (int i = 0; i < count; i++) {
+            out.println("<h1>Hello, World!</h1>");
+        }
     }
 }
